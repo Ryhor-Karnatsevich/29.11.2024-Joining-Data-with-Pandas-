54c1a3cd3ef70b1e2e0c6c1645f9a3306fd49fee
Ryhor-Karnatsevich <spamjamesmorgan@gmail.com>
2024-11-29 18:51:06 +0000

    1

--- Data Merging Basics.py
+++ Data Merging Basics.py
@@ -1,4 +1,9 @@
-import matplotli.pyplot as plt
+import matplotlib.pyplot as plt
 import pandas as pd
-data = pd.read_csv('table1')
+data = pd.read_csv('table1.csv')
+data1 = pd.read_csv('table2.csv')
 print(data)
+print(data1)
+
+data3 = data.merge(data1, on='name',suffixes=('_1','_2'))
+print(data3)
